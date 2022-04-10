65d7846b31890f862a7a113a2472bbd35368b941
moslemi <mhmoslemi2338@gmail.com>
2022-04-10 22:03:38 +0430

    same class size result

--- 4_train_SVM.py
+++ 4_train_SVM.py
@@ -1,127 +1,14 @@
-
-
 import os
 import scipy.io
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import svm
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.metrics import confusion_matrix
+from functions_SVM import *
 
 try: os.mkdir('results')
 except: pass
-
-def make_train_test_Data(features,label,pca_num,train_ratio):
-    Data=pd.DataFrame(np.concatenate([features,label],axis=1))
-    x =  Data.loc[:,[i for i in range(features.shape[1])]].values
-    # Standardizing the features
-    x = StandardScaler().fit_transform(x)
-    # PCA
-    pca = PCA(n_components=pca_num)
-    principalComponents = pca.fit_transform(x)
-    Data=pd.DataFrame(np.concatenate([principalComponents,label],axis=1))
-    # make train_test
-    train=Data.sample(frac=train_ratio,replace=False,random_state=0)
-    test=Data.drop(train.index)
-    return [train,test]
-
-def train_SVM(train, test):
-    x_train =  train.loc[:,[i for i in range(train.shape[1]-1)]].values
-    y_train = train.loc[:,[train.shape[1]-1]].values.ravel()
-
-    x_test=  test.loc[:,[i for i in range(test.shape[1]-1)]].values
-    y_test = test.loc[:,[test.shape[1]-1]].values.ravel()
-
-    clf = svm.SVC(kernel='rbf',decision_function_shape='ovo'  , class_weight='balanced'  ,max_iter=-1)
-    clf.fit(x_train, y_train)
-
-    ####### ACC on Test  ########
-    label_predict_Test=[]
-    for row in x_test:
-        ll=clf.predict([row])
-        label_predict_Test.append(ll)
-
-    label_predict_Test=np.array(label_predict_Test)
-    label_predict_Test=label_predict_Test.reshape(y_test.shape)
-    diff=(label_predict_Test==y_test)
-    acc=100*np.sum(diff)/len(y_test) 
-    CM=confusion_matrix(y_test, label_predict_Test)
-    return [CM,acc]
-
-def plot_CM(CM_in,name,is_save):
-    CM=CM_in.copy()
-    CM=CM/CM.sum(axis=1)[:,None]
-    (a,a)=CM.shape
-    ###### save confussion matrix as an image #####
-    fig=plt.figure(figsize=(16, 14))
-    plt.imshow(CM,  cmap=plt.cm.Blues);
-    thresh=CM.max()/2
-    for i in range(a):
-        for j in range(a):
-            number=CM[i, j].copy()
-            if number==float(0):
-                number=int(number)
-            number=round(number,3)
-            if(CM[i, j] > thresh) : color="white"
-            else: color="black"
-            fontweight='normal'
-            fontsize=20
-            if float(number)>0.5 : 
-                fontsize=22
-            plt.text(j, i,number ,horizontalalignment="center",color=color,fontsize=fontsize)
-    plt.xticks(np.arange(a), labels_name,fontsize='x-large',rotation=-30,fontweight='bold')
-    plt.yticks(np.arange(a),  labels_name,fontsize='x-large',fontweight='bold')
-    plt.title(name,fontsize=20,fontweight='bold'); plt.ylabel('True label',fontsize=20); plt.xlabel('Predicted label',fontsize=20);
-    if is_save:
-        fig.savefig('results/'+name+'.jpg', dpi=3*fig.dpi)
-        plt.close(fig)
-
-
-
-
-def statistics_CM(CM_in):
-    cm=CM_in.copy()
-    TP = np.diag(cm)
-    FP = np.sum(cm, axis=0) - TP
-    FN = np.sum(cm, axis=1) - TP
-
-    num_classes = len(labels_name)
-    TN = []
-    for i in range(num_classes):
-        temp = np.delete(cm, i, 0)    # delete ith row
-        temp = np.delete(temp, i, 1)  # delete ith column
-        TN.append(sum(sum(temp)))
-
-    acc=TP/np.sum(cm, axis=1)
-    precision = TP/(TP+FP)
-    recall = TP/(TP+FN)
-    specificity = TN/(TN+FP)
-    F1_score=2*precision*recall/(precision+recall)
-
-    acc=np.append(acc,np.average(acc,weights=np.sum(CM_in,axis=1)/np.sum(CM_in)))
-    precision=np.append(precision,np.average(precision,weights=np.sum(CM_in,axis=1)/np.sum(CM_in)))
-    recall = np.append(recall,np.average(recall,weights=np.sum(CM_in,axis=1)/np.sum(CM_in)))
-    specificity=np.append(specificity,np.average(specificity,weights=np.sum(CM_in,axis=1)/np.sum(CM_in)))
-    F1_score=np.append(F1_score,np.average(F1_score,weights=np.sum(CM_in,axis=1)/np.sum(CM_in)))
-
-
-    df=pd.DataFrame(
-        {"Accuracy":np.round(100*acc,2),
-        "Recall":np.round(100*recall,2),
-        "Precision":np.round(100*precision,2),
-        "Specificity":np.round(100*specificity,2),
-        "F1-score":np.round(100*F1_score,2)})
-    tmp=dict((v-1,k) for k,v in labels_dict.items())
-    tmp[5]='All classes'
-    return df.rename(index=tmp)
-
-
 #*******************************************************************
 #************************** Prepare Data ***************************
 #*******************************************************************
-
 
 ######## labels name ##########
 labels_name=os.listdir('features/Train_ILD')
@@ -152,7 +39,6 @@ label_ILD=np.array(label_ILD)
 label_ILD = np.reshape(label_ILD,(features_ILD.shape[0],1))
 
 
-
 ######## Talisman ##########
 label_Talisman=[]
 features_Talisman=[]
@@ -174,30 +60,27 @@ label_Talisman=np.array(label_Talisman)
 label_Talisman = np.reshape(label_Talisman,(features_Talisman.shape[0],1))
 
 
-####### statistics #####
+# ####### statistics #####
 Data_ILD=pd.DataFrame(np.concatenate([features_ILD,label_ILD],axis=1))
 Data_Talisman=pd.DataFrame(np.concatenate([features_Talisman,label_Talisman],axis=1))
-print('ILD class Distribution:')
-tmp=pd.DataFrame(Data_ILD[features_ILD.shape[1]].value_counts())
-tmp=tmp.rename(index=dict((v,k) for k,v in labels_dict.items()))
-tmp=tmp.rename(columns={48:'count'})
-print(tmp)
-print('\nTalisman Distribution Dist:')
-tmp=pd.DataFrame(Data_Talisman[features_Talisman.shape[1]].value_counts())
-tmp=tmp.rename(index=dict((v,k) for k,v in labels_dict.items()))
-tmp=tmp.rename(columns={48:'count'})
-print(tmp)
+# print('ILD class Distribution:')
+# tmp=pd.DataFrame(Data_ILD[features_ILD.shape[1]].value_counts())
+# tmp=tmp.rename(index=dict((v,k) for k,v in labels_dict.items()))
+# tmp=tmp.rename(columns={48:'count'})
+# print(tmp)
+# print('\nTalisman Distribution Dist:')
+# tmp=pd.DataFrame(Data_Talisman[features_Talisman.shape[1]].value_counts())
+# tmp=tmp.rename(index=dict((v,k) for k,v in labels_dict.items()))
+# tmp=tmp.rename(columns={48:'count'})
+# print(tmp)
 
 
 
 #**************************************************************
 #************************** Trainng ***************************
 #**************************************************************
-
-
 ratio=0.75
 pca_num=30
-
 
 [ILD_train,ILD_test]=make_train_test_Data(features_ILD,label_ILD,pca_num,ratio)
 [CM_ILD,acc_ILD]=train_SVM(ILD_train,ILD_test) 
@@ -207,20 +90,44 @@ print('\nAccuracy on ILD, RBF kernel,',pca_num,'PCA components: ',round(acc_ILD,
 [CM_Talisman,acc_Talisman]=train_SVM(Talisman_train,Talisman_test) 
 print('Accuracy on Talisman, RBF kernel,',pca_num,'PCA components: ',round(acc_Talisman,3),'%\n')
 
-
 ###### plot confusion matrix ######
-plot_CM(CM_ILD,'Confusion Matrix for ILD Data',True)
-plot_CM(CM_Talisman,'Confusion Matrix for Talisman Data',True)
-
-
-
+plot_CM(CM_ILD,labels_name,'Confusion Matrix for ILD Data',True)
+plot_CM(CM_Talisman,labels_name,'Confusion Matrix for Talisman Data',True)
 
 ##### acc values
-stat_ILD=statistics_CM(CM_ILD)
-stat_Talisman=statistics_CM(CM_Talisman)
-
+stat_ILD=statistics_CM(CM_ILD,labels_name,labels_dict)
+stat_Talisman=statistics_CM(CM_Talisman,labels_name,labels_dict)
 with open('results/ILD.txt', mode='w') as file_object:
     print(stat_ILD, file=file_object)
-
 with open('results/Talisman.txt', mode='w') as file_object:
     print(stat_Talisman, file=file_object)
+
+
+#************************** SAME CLASS SIZE ***************************
+Data_ILD_equ = equalize_class_size(Data_ILD)
+Data_Talisman_equ = equalize_class_size(Data_Talisman)
+label_ILD = np.array(Data_ILD_equ[48]).reshape(-1,1)
+features_ILD = Data_ILD_equ.drop(columns=48)
+label_Talisman = np.array(Data_Talisman_equ[48]).reshape(-1,1)
+features_Talisman = Data_Talisman_equ.drop(columns=48)
+
+[ILD_train,ILD_test]=make_train_test_Data(features_ILD,label_ILD,pca_num,ratio)
+[CM_ILD,acc_ILD]=train_SVM(ILD_train,ILD_test) 
+print('\nAccuracy on ILD, RBF kernel,',pca_num,'PCA components: ',round(acc_ILD,3),'%')
+
+[Talisman_train,Talisman_test]=make_train_test_Data(features_Talisman,label_Talisman,pca_num,ratio)
+[CM_Talisman,acc_Talisman]=train_SVM(Talisman_train,Talisman_test) 
+print('Accuracy on Talisman, RBF kernel,',pca_num,'PCA components: ',round(acc_Talisman,3),'%\n')
+
+###### plot confusion matrix ######
+plot_CM(CM_ILD,labels_name,'Confusion Matrix for ILD Data-same class size',True)
+plot_CM(CM_Talisman,labels_name,'Confusion Matrix for Talisman Data-same class size',True)
+
+##### acc values
+stat_ILD=statistics_CM(CM_ILD,labels_name,labels_dict)
+stat_Talisman=statistics_CM(CM_Talisman,labels_name,labels_dict)
+with open('results/ILD-same class size.txt', mode='w') as file_object:
+    print(stat_ILD, file=file_object)
+with open('results/Talisman-same class size.txt', mode='w') as file_object:
+    print(stat_Talisman, file=file_object)
+
