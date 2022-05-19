8c2a2bd9680828305c1c2cb08abba0a32d533bfd
moslemi <mhmoslemi2338@gmail.com>
2022-05-19 19:35:39 +0430

    feature extract final

--- Implement/step1_graph_feature.m
+++ Implement/step1_graph_feature.m
@@ -1,11 +1,11 @@
 
 
 clear all; clc; close all
-dest_dir='texture_features';
+dest_dir='texture_features2';
 
-%*************************************
-%%%%%%%%%%% Prepare folders %%%%%%%%%%
-%*************************************
+%--------------------------------------
+%---------- Prepare folders -----------
+%--------------------------------------
 addpath matlabTools/
 cnt=1;
 lables=split(strip(ls('data/')));
@@ -13,77 +13,61 @@ lables=split(strip(ls('data/')));
 if ~ isdir(dest_dir)
     mkdir(dest_dir);
 end
-if ~ isdir(join([dest_dir,'/graph']))
-    mkdir(join([dest_dir,'/graph']));
+if ~ isdir(join([dest_dir,'/']))
+    mkdir(join([dest_dir,'/']));
 end
 
 path={};
 for k=1:length(lables)
     ll=lables{k};
     path=[path; join(['data/',ll])];
-    if ~ isdir(join([dest_dir,'/graph/',ll]))
-        mkdir(join([dest_dir,'/graph/',ll]))
+    if ~ isdir(join([dest_dir,'/',ll]))
+        mkdir(join([dest_dir,'/',ll]))
     end
 end
-%*************************************
-%%%%%%%%%%% EXTRACT FEATURES %%%%%%%%%
-%*************************************
 
+%--------------------------------------
+%---------- EXTRACT FEATURES ----------
+%--------------------------------------
 for k=1:length(path)
     ll=path{k};
     images=split(strip(ls(ll)));
     for k2=1:length(images)
         filename=join([ll,'/',images{k2}]);
-        img = uint8(imread(filename));
- 
-        %%%%%% HVG , IVG
+        img = imread(filename);
+        %%%%%% HVG , IVG %%%%%%
         feature_vector=[];
         symmetric=0;
-        for I = {img ,255-img}
+        for I = {uint8(img) ,255-uint8(img)}
             symmetric=symmetric+1;
             I=cell2mat(I);
             for method={'horizontal','natural'}
                 for lattice={true , false}    
-                     if (symmetric==2) && (cell2mat(lattice)==false)
-                         continue
-                     end
-                     if (symmetric==1) && (length(cell2mat(method))==7) && (cell2mat(lattice)==true)
-                         continue
-                     end
-
+                    if (symmetric==2) && (cell2mat(lattice)==false)
+                        continue
+                    end
+                    if (symmetric==1) && (length(cell2mat(method))==7) && (cell2mat(lattice)==true)
+                        continue
+                    end
                     Edge_list=imageVisibilityGraph(I,cell2mat(method),cell2mat(lattice));
                     G = graph(Edge_list(:,1),Edge_list(:,2));
-% % %                     global_feature=global_feature_extract(G);
                     Z=visibilityPatches(I,1,cell2mat(method));
-%                     Z2=visibilityPatches(I,2,cell2mat(method));
-%                     Z2=hist(Z2,64);
-%                     Z3=visibilityPatches(I,3,cell2mat(method));
-%                     Z3=hist(Z3,64);
                     feature_vector=[feature_vector Z];
-                    
-                
                 end
             end
-            %%%%%% wavelet
-%             wavelet_feature=wavelet_feature_extractor(I);
-%             feature_vector=[feature_vector wavelet_feature];
         end  
-       
-        
-        
-        savepath=replace(replace(filename,'.png','.mat'),'data',join([dest_dir,'/graph']));
+        %%%%%% wavelet %%%%%%
+        wavelet_feature1=wavelet_feature_extractor(double(img));
+        wavelet_feature2=wavelet_feature_extractor(255-double(img));
+        feature_vector=[feature_vector wavelet_feature1 wavelet_feature2];
+        %%%%% Saving result %%%%%%
+        savepath=replace(replace(filename,'.png','.mat'),'data',dest_dir);
         save(savepath,'feature_vector');
-        %print progress
+        %%%%% print progress %%%%%%
         cnt=cnt+1;
         if mod(cnt,296)==0
             disp(join(['progress:',num2str(cnt/296),' % ']))
         end                 
     end     
 end
-
-
-
-
-
-
 
