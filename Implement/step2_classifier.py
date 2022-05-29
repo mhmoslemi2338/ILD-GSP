ac6ada39b60ecca2797ad526c80bebb24ce43551
moslemi <mhmoslemi2338@gmail.com>
2022-05-29 20:12:55 +0430

    add : latex format for CM

--- Implement/step2_classifier.py
+++ Implement/step2_classifier.py
@@ -52,8 +52,6 @@ label_ILD = np.reshape(label_ILD,(features.shape[0],1))
 
 #------ Prepare Data ------
 Data=pd.DataFrame(np.concatenate([features, label_ILD],axis=1))
-Data[Data.shape[1]-1].value_counts()
-
 Train=Data.sample(frac=1,random_state=5) ## shuffle
 x_train =  Train.loc[:,[i for i in range(Train.shape[1]-1)]].values
 y_train = Train.loc[:,[Train.shape[1]-1]].values.ravel()
@@ -61,7 +59,7 @@ y_test_all=np.array([])
 y_prediction_all=np.array([])
 
 
-skfolds = StratifiedKFold(n_splits=10, shuffle=True,random_state=42)
+skfolds = StratifiedKFold(n_splits=5, shuffle=True,random_state=42)
 for train_index, test_index in skfolds.split(x_train, y_train):
     #------ Train , Test folds ------
     x_train_folds = x_train[train_index]
@@ -89,7 +87,7 @@ for train_index, test_index in skfolds.split(x_train, y_train):
 
     y_test_all=np.concatenate([y_test_all,y_test_fold])
     y_prediction_all=np.concatenate([y_prediction_all,label_predict_test])
-
+    
 
 #------ result ------
 cm = ConfusionMatrix(actual_vector=y_test_all, predict_vector=y_prediction_all)
@@ -102,8 +100,17 @@ recall=np.array(list((cm.TPR).values()))
 true_negative_rate=np.array(list((cm.TNR).values()))
 AUC=np.array(list((cm.AUC).values()))
 F1=np.array(list((cm.F1).values()))
-overall_accuracy=round(100*cm.Overall_ACC,2)
-overal_F1=round(100*cm.F1_Macro,2)
+
+overall_accuracy=str(round(100*cm.Overall_ACC,2))
+overal_precision=str(round(100*cm.PPV_Micro,2))
+overal_recall=str(round(100*cm.TPR_Micro,2))
+overal_true_negative_rate=str(round(100*cm.TNR_Micro,2))
+overal_AUC='-'
+overal_F1=str(round(100*cm.F1_Micro,2))
+
+overal=[overall_accuracy , overal_precision , overal_recall 
+        , overal_true_negative_rate , overal_AUC , overal_F1]
+
 
 df=pd.DataFrame(
     {"Accuracy":np.round(100*accuracy,2),
@@ -111,12 +118,11 @@ df=pd.DataFrame(
     "Precision":np.round(100*precision,2),
     "TN rate":np.round(100*true_negative_rate,2),
     "AUC":np.round(100*AUC,2),
-    "F1":np.round(100*F1,2)})
+    "F1-score":np.round(100*F1,2)})
 df=df.rename(index=dict((v-1,k) for k,v in labels_dict.items()))
+df.loc['Overal']=overal
 with open('result/ILD_Test.txt', mode='w') as file_object:
     print(df, file=file_object)
-    print('\n\t**** overal accuracy: '+str(overall_accuracy)+' % ', file=file_object)
-    print('\t**** overal F1-score (Macro): '+str(overal_F1)+' %', file=file_object)
 
 print('\nFeature vector length:',Train.shape[1]-1)
 print('SVM, RBF kernel,PCA components: ',pca_num)
@@ -124,8 +130,42 @@ print('\tAccuracy on Test: ' ,overall_accuracy,'%')
 print('\tF1_score on Test: ',overal_F1,'%')
 
 
-
-
-
-
+##### LATEX result format
+CM_label=['H (Healthy)','GG (Ground galss)','M (Micronodules)','E (Emphysema)','F (Fibrosis)']
+with open('result/CM_latex.txt', mode='w') as file_object:
+    for j,row in enumerate(CM):
+        print(CM_label[j]+' & ',end='', file=file_object)
+        for i,row2 in enumerate(row):
+            if i!=len(row)-1:
+                print(str(row2)+' &',end=' ', file=file_object)
+            else:
+                if j!=len(row)-1:
+                    print(str(row2)+" \\\ ", file=file_object)
+                else:
+                    print(str(row2)+" \\\ \\bottomrule", file=file_object)
+######
+with open('result/ILD_Test.txt') as f:
+    data=f.readlines()
+for i in range(len(data)):
+    data[i]=data[i].strip().split()
+data=data[1:]
+data[5][0]=data[5][0] + ' & '
+for i in range(6):
+    if i!=5:
+        data[i][0]=CM_label[i].split()[0] + ' & '
+    for j in range(5):
+        if i==5 and j==4 : 
+            data[i][j+1]=data[i][j+1]+' & '
+            continue
+        data[i][j+1]=data[i][j+1]+'\% & '
+    data[i][6]=data[i][6]+ '\% \\\ '
+data[-2][-1]=data[-2][-1]+"  \\bottomrule"
+data.append(['\\vspace{-0.3cm} \\\ '])
+with open('result/acc_latex.txt', mode='w') as file_object:
+    for row in data[:-2]:
+        print("".join(row),file=file_object)
+    print("\\vspace{-0.25cm} \\\ ",file=file_object)
+    print("".join(data[-2]),file=file_object)
+    print("".join(data[-1]),file=file_object)
+    print("\\bottomrule",file=file_object)
 
